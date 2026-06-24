b484174dd203a2c119bfce8c3152045216a640ee
terigele <21010408@anabuki-college.ac.jp>
2026-06-25 00:15:41 +0900

    helpersの追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'simditor/styles/simditor.css';
+import Simditor from 'simditor';
